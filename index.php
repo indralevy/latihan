a333fa199a152fc3d239da2c220b2fb15c64fd9e
indralevy <indrajitalepro@gmail.com>
2022-02-18 16:15:33 +0700

    Latihan pake variable & array

--- index.php
+++ index.php
@@ -4,7 +4,29 @@
         <h1> First PHP Commit</h1>
 
         <?php
-        echo "HALO GAN!!!"
+        $color = "red";
+        $Color = "green";
+        $b = 5;
+        $c = 5;
+        $Z;
+        $a = 69;
+        $x = 1;
+        $y = 1;
+        $z = $x + $y;
+        echo "hasil = ".$z."<br>";
+        echo "HALO GAN!!!".$color."<br>";
+        ECHO "Hello World2!".$Color."<br>";
+        
+        function fungsisatu($a=69) {
+            // kie variable global nang jero function. global $Z, $b, $c;
+            // $Z = $b + $c; 
+            // nek nang ngisor kie nganggo array
+            $GLOBALS['Z'] = $GLOBALS['b'] + $GLOBALS['c'];
+            echo "<p>This variable X inside fungsisatu function = $a</p>";
+        }
+        fungsisatu();
+        echo "<p>This is variable x outside fungsisatu function = $a</P>";
+        echo "<p>The variable and array Z is = $Z</p>";
         ?>
 
     </body>
